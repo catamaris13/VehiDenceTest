fa92a8ca00d8cd13993d733b4b9cfd9913efadd7
sasa_mezo <sasamezo77@gmail.com>
2024-05-04 12:44:22 +0300

    nu merge Log out

--- Front-End/vehidence-front/src/Home.js
+++ Front-End/vehidence-front/src/Home.js
@@ -1,19 +1,36 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
+  const login = localStorage.getItem('islogin');
+  console.log("login value:", login);
+
+  let destination;
+  if(Boolean(login)){
+    destination = '/myaccount';
+  }
+  else{
+    destination = '/login'
+  }
+
   return (
     <div className="home">
       <nav className="navbar">
-      <h1>Vehi Dence</h1>
-      <div className="links">
-        <a href="/myaccount" style={{ 
-          color: 'white', 
-          backgroundColor: '#3c009d',
-          borderRadius: '10px', 
-          padding: '8px 20px'
-        }}>My account</a>
-      </div>
-    </nav>
+        <h1>Vehi Dence</h1>
+        <div className="links">
+          <Link
+            to={destination}
+            style={{
+              color: "white",
+              backgroundColor: "#3c009d",
+              borderRadius: "10px",
+              padding: "8px 20px",
+            }}
+            >
+            My account
+          </Link>
+        </div>
+      </nav>
     </div>
   );
-}
- 
+};
 export default Home;
